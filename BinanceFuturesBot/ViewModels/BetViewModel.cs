515dc969330130bf0c3dc3d3fce73cb40299c718
VLola <valiklolanew@gmail.com>
2022-12-14 03:04:14 +0200

    fixed bugs added chart history

--- BinanceFuturesBot/ViewModels/BetViewModel.cs
+++ BinanceFuturesBot/ViewModels/BetViewModel.cs
@@ -1,18 +1,20 @@
 ﻿using Binance.Net.Clients;
 using Binance.Net.Enums;
+using Binance.Net.Interfaces;
 using Binance.Net.Objects.Models.Futures;
+using BinanceFuturesBot.Command;
 using BinanceFuturesBot.Models;
-using CryptoExchange.Net.CommonObjects;
+using BinanceFuturesBot.Views;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BinanceFuturesBot.ViewModels
 {
     public class BetViewModel
     {
+        private string _pathLog = $"{Directory.GetCurrentDirectory()}/log/";
         public BetModel BetModel { get; set; } = new();
         public BinanceClient? Client { get; set; }
         public BetViewModel(BinanceFuturesUsdtTrade binanceFuturesUsdtTrade, BinanceClient? client) {
@@ -30,6 +32,78 @@ namespace BinanceFuturesBot.ViewModels
             BetModel.Commission = binanceFuturesUsdtTrade.Fee;
             BetModel.Total = (binanceFuturesUsdtTrade.RealizedPnl - binanceFuturesUsdtTrade.Fee);
             BetModel.OrderSide = binanceFuturesUsdtTrade.Side;
+        }
+
+        private RelayCommand? _showChartCommand;
+        public RelayCommand ShowChartCommand
+        {
+            get
+            {
+                return _showChartCommand ?? (_showChartCommand = new RelayCommand(obj => {
+                    ShowChart();
+                }));
+            }
+        }
+        private void ShowChart()
+        {
+            DateTime startTime = BetModel.Time.AddHours(-1);
+            DateTime endTime = BetModel.Time.AddHours(1);
+
+            List<IBinanceKline> klines = Klines(KlineInterval.FiveMinutes, startTime, endTime);
+            List<(double x, double y)> points = GetUserTrades(startTime, endTime);
+            ChartWindow chartWindow = new ChartWindow(klines, points);
+            chartWindow.Show();
+        }
+        public List<IBinanceKline> Klines(KlineInterval interval, DateTime startTime, DateTime endTime)
+        {
+            try
+            {
+                var result = Client.UsdFuturesApi.ExchangeData.GetKlinesAsync(symbol: BetModel.Symbol, interval: interval, startTime: startTime, endTime: endTime).Result;
+                if (!result.Success) WriteLog("Error Klines");
+                else
+                {
+                    return result.Data.ToList();
+                }
+            }
+            catch (Exception eX)
+            {
+                WriteLog($"Klines {eX.Message}");
+            }
+            return null;
+        }
+        private List<(double x, double y)> GetUserTrades(DateTime startTime, DateTime endTime)
+        {
+            try
+            {
+                var result = Client.UsdFuturesApi.Trading.GetUserTradesAsync(symbol: BetModel.Symbol, startTime: startTime, endTime: endTime).Result;
+                if (!result.Success) WriteLog($"Failed GetUserTrades: {result.Error?.Message}");
+                else
+                {
+                    if (result.Data.ToList().Count > 0)
+                    {
+                        List<(double x, double y)> list = new();
+
+                        foreach (var item in result.Data.ToList())
+                        {
+                            list.Add((item.Timestamp.ToOADate(), Decimal.ToDouble(item.Price)));
+                        }
+                        return list;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                WriteLog($"GetUserTrades: {ex.Message}");
+            }
+            return null;
+        }
+        private void WriteLog(string text)
+        {
+            try
+            {
+                File.AppendAllText(_pathLog + "_BETMODEL", $"{DateTime.Now} {BetModel.Symbol} {text}\n");
+            }
+            catch { }
         }
     }
 }
